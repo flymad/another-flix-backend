b3709ccb36a033fb728969caadf1f5a371a42826
yaztaleb <yaztaleb@gmail.com>
2022-06-14 15:25:51 -0500

    Folder structures created. User & Movie models created.

---
+++ server.js
@@ -0,0 +1,12 @@
+// Imports
+const express = require('express')
+const app = express()
+require('dotenv').config()
+
+// ===== ROUTES =====
+// app.use('/', )
+
+// ===== PORT =====
+const port = 8080
+
+app.listen(port, () => console.log(`Express app running on port ${port}`))
